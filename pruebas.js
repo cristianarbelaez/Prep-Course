a36d329db22a24821457baeabf889a37950bc302
Cristian Arbelaez <108770764+cristianarbelaez@users.noreply.github.com>
2022-07-12 01:39:05 -0500

    guardar

---
+++ pruebas.js
@@ -0,0 +1,15 @@
+function esDiezOCinco(numero) {
+    // Devuelve "true" si "numero" es 10 o 5
+    // De lo contrario, devuelve "false"
+    // Tu código:
+    return numero == 5 || numero == 10
+  }
+
+esDiezOCinco(4)
+
+return "hola"
+
+
+
+
+
